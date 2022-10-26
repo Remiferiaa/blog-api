f3eca199816dd05bb47d707bfc47b53d039ca432
Remiferiaa <slong819@hotmail.com>
2022-10-26 19:17:29 +0800

    mongoose connection

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,4 @@
-import 'dotenv'
+import 'dotenv/config'
 import express from 'express'
 import mongoose from 'mongoose'
 import passport from 'passport'
@@ -6,6 +6,10 @@ import local from 'passport-local'
 import jwtstrat from 'passport-jwt'
 
 const app = express()
+const mongoDB: string = process.env.DB!
+mongoose.connect(mongoDB);
+const db = mongoose.connection; 
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.get('/' , (req , res) => {
     res.send('e')
@@ -13,4 +17,6 @@ app.get('/' , (req , res) => {
 
 app.listen(3000, () =>
   console.log(`Example app listening on port 3000!`),
-);
+); 
+
+
