875dda31172023957e5c20919a8c325d9598b7e7
Remiferiaa <slong819@hotmail.com>
2022-10-28 17:49:03 +0800

    parse json request

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,19 +4,28 @@ import mongoose from 'mongoose'
 import passport from 'passport'
 import local from 'passport-local'
 import jwtstrat from 'passport-jwt'
+import router from './routes/auth'
+
 
 const app = express()
 const mongoDB: string = process.env.DB!
 mongoose.connect(mongoDB);
 const db = mongoose.connection; 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use('/api', router)
 
 app.get('/' , (req , res) => {
-    res.send('e')
+    res.json({
+      message:' hi asdfasdf dd'
+    })
 })
 
 app.listen(3000, () =>
   console.log(`Example app listening on port 3000!`),
 ); 
+
+
 
 
