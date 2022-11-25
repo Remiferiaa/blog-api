305d0361c7c176c3103e00d232f32bc4e06fe455
Remiferiaa <slong819@hotmail.com>
2022-11-25 23:34:21 +0800

    login function refactor

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,23 +1,31 @@
 import 'dotenv/config'
-import user, { IUser } from '../models/user';
+import user from '../models/user';
 import { Request, Response, NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
 
+const user_login_post = async (req: Request, res: Response, next: NextFunction) => {
+    const owner = await user.findOne({ username: req.body.username}).exec()
 
-const user_login_post = (req: Request, res: Response, next: NextFunction) => {
-    user.findOne({ username: req.body.username, password: req.body.password }, (err: Error, user: IUser) => {
-        if (err || !user) {
-            return res.sendStatus(403)
-        } else {
-            jwt.sign({ user }, process.env.SECRET!, {}, (err, token) => {
-                res.json({
-                    username: user.username,
-                    token
-                })
-            })
-        }
+    if (!owner) {
+        return res.status(401).send({
+            'error': '401',
+            'message': 'Invalid User'
+        })
+    }
+
+    if (owner?.password !== req.body.password) {
+        return res.status(401).send({
+            'error': '401',
+            'message': 'Wrong Password'
+        })
+    }
+
+    const token = jwt.sign({owner}, process.env.SECRET!)
+
+    return res.json({
+        username: owner?.username,
+        token
     })
 }
-
-export default { user_login_post }
+export default { user_login_post } 
 
