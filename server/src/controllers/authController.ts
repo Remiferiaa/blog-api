5810da69f94bfc1cf85d670a2c0fc758131574ba
Remiferiaa <slong819@hotmail.com>
2022-11-26 22:31:32 +0800

    removed quotation for error

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -8,15 +8,15 @@ export const user_login_post = async (req: Request, res: Response, next: NextFun
 
     if (!owner) {
         return res.status(401).json({
-            'error': '401',
-            'message': 'Invalid User'
+            error: 401,
+            message: 'Invalid User'
         })
     }
 
     if (owner?.password !== req.body.password) {
         return res.status(401).json({
-            'error': '401',
-            'message': 'Wrong Password'
+            error: 401,
+            message: 'Wrong Password'
         })
     }
 
