c7732f62287ed115efc7f3869489ba4b39fe56b3
Remiferiaa <slong819@hotmail.com>
2022-11-30 23:29:50 +0800

    post method added for posts model

--- server/src/models/posts.ts
+++ server/src/models/posts.ts
@@ -1,24 +1,31 @@
 import mongoose from 'mongoose'
 import { Schema, Types } from 'mongoose';
+import message from './message';
 
-interface IPosts {
+export interface IPosts {
     postedAt: Date;
     postTitle: String;
     postBody: String;
     postComments: Types.ObjectId;
 }
-  
+
 const PostSchema = new Schema<IPosts>({
     postedAt: { type: Date, default: Date.now },
-    postTitle: {type: String, required:true}, 
-    postBody: {type: String, required:true}, 
-    postComments: {type: Schema.Types.ObjectId, ref: 'Message'},
+    postTitle: { type: String, required: true },
+    postBody: { type: String, required: true },
+    postComments: [{ type: Schema.Types.ObjectId, ref: 'Message' }],
 })
+
+function removeLinkDoc(doc: IPosts) {
+    message.remove({ _id: { $in: doc.postComments}})
+}
 
 PostSchema
     .virtual('published')
-    .get(function() {    
+    .get(function () {
         return this.postedAt.toLocaleDateString()
-})
+    })
+
+PostSchema.post('remove', removeLinkDoc)
 
 export default mongoose.model<IPosts>('Post', PostSchema)
