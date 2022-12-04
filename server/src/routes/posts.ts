e1726e43ae931778bdd9a5403572daf26c93c482
Remiferiaa <slong819@hotmail.com>
2022-12-04 21:57:22 +0800

    change callback for posts route

--- server/src/routes/posts.ts
+++ server/src/routes/posts.ts
@@ -1,11 +1,11 @@
 import express from 'express'
-import { post_commentlist_get, post_commentlist_post, post_comment_delete, post_comment_get, post_delete, post_get, post_put } from '../controllers/postController';
+import { postlist_get, postlist_post, post_commentlist_get, post_commentlist_post, post_comment_delete, post_comment_get, post_delete, post_get, post_put } from '../controllers/postController';
 
 const router = express.Router();
 
 router.route('/posts')
-    .get(post_commentlist_get)
-    .post(post_commentlist_post)
+    .get(postlist_get)
+    .post(postlist_post)
 
 router.route('/posts/:postid')
     .get(post_get)
