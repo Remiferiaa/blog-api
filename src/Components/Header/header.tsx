a238f07da0b1010e4fb8c3e0dae638703948ed22
Remiferiaa <slong819@hotmail.com>
2022-12-15 21:58:24 +0800

    added header tag

--- src/Components/Header/header.tsx
+++ src/Components/Header/header.tsx
@@ -2,9 +2,11 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <Link to='/'>
-            Blog
-        </Link>
+        <header>
+            <Link to='/'>
+                Blog
+            </Link>
+        </header>
     )
 }
 
