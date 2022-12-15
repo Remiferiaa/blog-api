2ba97df578e559013d8f7f6c10c85df4d405043e
Remiferiaa <slong819@hotmail.com>
2022-12-16 00:38:11 +0800

    hero component done

---
+++ client/src/Components/Hero/hero.tsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+    return (
+        <div>
+            <h1> Welcome to the blog.</h1>
+            <p>Click on any of the blog posts below to read more</p>
+        </div>
+    )
+}
+
+export default Hero
