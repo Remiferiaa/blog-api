1a10e5940d4f7220acd7da0c061694909707658e
Remiferiaa <slong819@hotmail.com>
2022-12-19 01:16:02 +0800

    slight changes

--- src/Components/CMS/newPost.tsx
+++ src/Components/CMS/newPost.tsx
@@ -1,16 +1,28 @@
-import React, {useState } from 'react'
-import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import useCms from '../../Hook/useCms'
 
+
 const NewPost = () => {
-    const curPost = useParams()
+    const navigate = useNavigate()
     const { newPosts } = useCms()
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
+    const [err, setErr] = useState('')
 
-    function submit(e: React.FormEvent) {
-        e.preventDefault()
-        newPosts(curPost.id!, title, body)
+    async function submit(e: React.FormEvent) {
+        try {
+            e.preventDefault()
+            await newPosts(title, body)
+            navigate('/')
+        } catch (err) {
+            if (axios.isAxiosError(err)) {
+                setErr(err.response?.data.message)
+            } else {
+                console.error(err)
+            }
+        }
     }
 
     return (
@@ -20,8 +32,9 @@ const NewPost = () => {
                     <label htmlFor="postTitle">Title:</label>
                     <input type='text' id='postTitle' name='postTitle' required autoComplete='off' value={title} onChange={(e) => setTitle(e.target.value)} />
                     <label htmlFor="postBody">Content:</label>
-                    <textarea id='postBody' name='postBody' required value={body} onChange={(e) => setBody(e.target.value)}/>
+                    <textarea id='postBody' name='postBody' required value={body} onChange={(e) => setBody(e.target.value)} />
                     <button className='edit-btn'>Post</button>
+                    {err ? <p>{err}</p> : <></>}
                 </form>
             </div>
         </>
