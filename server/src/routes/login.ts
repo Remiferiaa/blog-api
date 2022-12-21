dfe23378b80b6eb9154f9e68a83dfb292bfe6c7e
Remiferiaa <slong819@hotmail.com>
2022-12-21 22:25:54 +0800

    added verify route

--- server/src/routes/login.ts
+++ server/src/routes/login.ts
@@ -1,9 +1,16 @@
 import express from 'express'
-import {user_login_post} from '../controllers/authController'
+import {authVer, user_login_post} from '../controllers/authController'
+import { Request, Response, NextFunction } from 'express'
 
 const router = express.Router();
 
 router.post('/login', user_login_post)
+
+router.route('/verify').get(authVer, (req: Request, res: Response, next: NextFunction) => {
+    res.json({
+        message: 'Verified'
+    })
+})
 
 export default router;
 
